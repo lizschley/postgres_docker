76473be035d97c50158651baf2124a8dd4c12119
Liz Schley <liz.schley@advance-auto.com>
2023-02-23 17:32:13 -0500

    working with first table from tuturial

---
+++ src/main/java/liz/postgres_demo/Student.java
@@ -0,0 +1,44 @@
+package liz.postgres_demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.Locale;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "Student")
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private Integer age;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
